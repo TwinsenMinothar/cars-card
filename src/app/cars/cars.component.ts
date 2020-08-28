b595a3a8076a40af2d64e1a5f6ea2cf93b3270d3
Camillo Rinaldi <camillo_rinaldi@yahoo.com.br>
2020-08-28 02:05:49 -0300

    Show cars with img

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,54 +1,59 @@
-import { Component, OnInit } from "@angular/core";
-import { ModelsForMake } from "../models-for-make";
-import { ActivatedRoute } from "@angular/router";
-import { JsonVehiclesServiceService } from "../json-vehicles-service.service";
-import { Car } from "../car";
-import { Pixbay } from "../pixbay";
-import { ImageService } from "../image.service";
+import {Component, OnInit} from '@angular/core';
+import {ModelsForMake} from '../models-for-make';
+import {ActivatedRoute} from '@angular/router';
+import {JsonVehiclesServiceService} from '../json-vehicles-service.service';
+import {Car} from '../car';
+import {Pixbay} from '../pixbay';
+import {ImageService} from '../image.service';
 
 @Component({
-  selector: "app-cars",
-  templateUrl: "./cars.component.html",
-  styleUrls: ["./cars.component.scss"]
+    selector: 'app-cars',
+    templateUrl: './cars.component.html',
+    styleUrls: ['./cars.component.scss']
 })
 export class CarsComponent implements OnInit {
-  modelsForMake: ModelsForMake;
-  cars: Car[];
-  pixbayImage: Pixbay;
-  private maker: string;
-  constructor(
-    private jsonVehiclesService: JsonVehiclesServiceService,
-    private route: ActivatedRoute,
-    private imageService: ImageService
-  ) {}
+    modelsForMake: ModelsForMake;
+    cars: Car[];
+    showUpCars: Car[];
+    pixbayImage: Pixbay;
+    private maker: string;
 
-  ngOnInit() {
-    this.maker = this.route.snapshot.paramMap.get("maker");
-    this.getCars();
-  }
+    constructor(
+        private jsonVehiclesService: JsonVehiclesServiceService,
+        private route: ActivatedRoute,
+        private imageService: ImageService
+    ) {
+    }
 
-  getCars() {
-    this.jsonVehiclesService
-      .getCars(this.maker)
-      .subscribe(json => ((this.cars = json.Results), this.getImage()));
-  }
+    ngOnInit() {
+        this.showUpCars = [];
+        this.maker = this.route.snapshot.paramMap.get('maker');
+        this.getCars();
+    }
 
-  getCarsLength() {
-    return this.cars && this.cars.length ? this.cars.length : [];
-  }
+    getCars() {
+        this.jsonVehiclesService
+            .getCars(this.maker)
+            .subscribe(json => ((this.cars = json.Results), this.getImage()));
+    }
 
-  getImage() {
-    this.cars.forEach(car => {
-      this.imageService
-        .getImage(`${car.Make_Name}+${car.Model_Name}`)
-        .subscribe(json => {
-          if (json.total == 0) {
-            car.imgLink = "https://pixabay.com/static/img/logo_square.png";
-          } else {
-            let random = Math.random() * json.hits.length;
-            car.imgLink = json.hits[Math.floor(random)].webformatURL;
-          }
+    getCarsLength() {
+        return this.cars && this.cars.length ? this.cars.length : [];
+    }
+
+    getImage() {
+        this.cars.forEach(car => {
+            this.imageService
+                .getImage(`${car.Make_Name}+${car.Model_Name}`)
+                .subscribe(json => {
+                    if (json.total === 0) {
+                        car.imgLink = 'https://pixabay.com/static/img/logo_square.png';
+                    } else {
+                        const random = Math.random() * json.hits.length;
+                        car.imgLink = json.hits[Math.floor(random)].webformatURL;
+                        this.showUpCars.push(car);
+                    }
+                });
         });
-    });
-  }
+    }
 }
